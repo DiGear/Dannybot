e3a7afdd409fbd60d69b8dc50f504a7bb7ac5190
Danny <zestychair@gmail.com>
2023-01-20 01:51:50 -0500

    ezogaming shit

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 # this is where most of the bullshit will be taking place
 import os
-
+import random
+import re
 
 dannybot = os.getcwd()
 
@@ -21,3 +22,48 @@ def repack_gif_JPG():
     os.system(f'ffmpeg -i "{dannybot}\\cache\\ffmpeg\\output\\temp%04d.png.jpg" -lavfi "scale=256x256,fps=25,palettegen=max_colors=256:stats_mode=diff" {dannybot}ffmpeg\\output\\palette.png -y')
     os.system(f'ffmpeg -i "{dannybot}\\cache\\ffmpeg\\output\\temp%04d.png.jpg" -i "{dannybot}ffmpeg\\output\\palette.png" -lavfi "fps=25,mpdecimate,paletteuse=dither=none" -fs 8M "{dannybot}\\cache\\ffmpeg_out.gif" -y')
     return
+
+
+def cleanup_ffmpeg():
+    for file in os.listdir(f'{dannybot}\\cache\\ffmpeg'):
+        if '.png' in file:
+            os.remove(f'{dannybot}\\cache\\ffmpeg\\{file}')
+    for file in os.listdir(f'{dannybot}\\cache\\ffmpeg\\output'):
+        if '.png' in file:
+            os.remove(f'{dannybot}\\cache\\ffmpeg\\output\\{file}')
+
+
+def fileCount(folder):
+    total = 0  # set total to 0 to begin with
+    # recursively walk down the folder passed into the function
+    for files in os.walk(folder):
+
+        total += len(files)  # add each file found to total
+    return total  # send the total
+
+
+def ezogaming_regex(datalist, dataentry):
+    # ezogaming if you would like to add comments to this catastrophe, be my guest
+    # this may even be rewritten completely by the time you are reading this
+    with open(f"{dannybot}\\ezogaming\\{datalist}_char") as f:
+        entry = f.readlines()
+        entry = [x.rstrip() for x in entry]
+    with open(f"{dannybot}\\ezogaming\\{datalist}_checker") as f:
+        entryalias = f.readlines()
+        entryalias = [x.rstrip() for x in entryalias]
+    aru = " ".join(dataentry[:])
+    inp = re.sub("[^a-z]", "", aru.lower())
+    sort = [0] * len(entry)
+    for i in range(0, len(entry)):
+        sort[i] = i
+    random.shuffle(sort)
+    for i2 in range(0, len(entry)):
+        inputStripped = inp.strip()
+        aliasStripped = re.sub(
+            "[^a-z]", "", entryalias[sort[i2]].lower().strip())
+        entrystripped = re.sub("[^a-z]", "", entry[sort[i2]].lower().strip())
+        if (inputStripped in entrystripped) or inputStripped in aliasStripped:
+            break
+    sort[i2]
+    results = entry[sort[i2]]
+    return results
