a1534dde531ae375ef66e6193ea52db5a4a79a98
Danny <zestychair@gmail.com>
2023-01-20 12:29:13 -0500

    comments for the meme command

--- data.py
+++ data.py
@@ -268,16 +268,20 @@ async def resolve_args(ctx, args, attachments):
 # primary function of the meme command
 # take an image and put centered and outlined impact font text with a black outline over the top and bottom of the image
 def make_meme(Top_Text, Bottom_Text, path):
+    # open image in PIL
     img = PIL.Image.open(path)
-    imageSize = img.size
 
-    # fix font size calc
+    # fixed font size calc
+    # proportionally scales the font to the size of the image, and make sure it doesn't equal 0
+    imageSize = img.size
     fontSize = int(imageSize[1]/5)
     if fontSize  <= 0:
         fontSize = 1
 
     font = ImageFont.truetype(
         f"{dannybot}\\assets\\impactjpn.otf", fontSize)
+
+    # scale and position the text
     topTextSize = font.getsize(Top_Text)
     bottomTextSize = font.getsize(Bottom_Text)
     topTextPositionX = (imageSize[0]/2) - (topTextSize[0]/2)
@@ -285,9 +289,11 @@ def make_meme(Top_Text, Bottom_Text, path):
     bottomTextPositionX = (imageSize[0]/2) - (bottomTextSize[0]/2)
     bottomTextPositionY = imageSize[1] - bottomTextSize[1]
     bottomTextPosition = (bottomTextPositionX, bottomTextPositionY - 10)
-    draw = ImageDraw.Draw(img)
 
     # FIX THE FUCKING STROKE SIZE
+    # idk why i never bothered to calculate stroke size like this
+    # it divides the size of both top and bottom text by 75 and uses that as the stroke size
+    # also we make sure it doesn't equal 0
     top_outline = int((topTextSize[0]//75))
     bottom_outline = int((bottomTextSize[0]//75))
     if top_outline <= 0:
@@ -295,50 +301,65 @@ def make_meme(Top_Text, Bottom_Text, path):
     if bottom_outline <= 0:
         bottom_outline = 1
 
+    # draw the text
+    draw = ImageDraw.Draw(img)
     draw.text(topTextPosition, Top_Text, (255, 255, 255), font=font,
               stroke_width=top_outline, stroke_fill=(0, 0, 0))
     draw.text(bottomTextPosition, Bottom_Text, (255, 255, 255),
               font=font, stroke_width=bottom_outline, stroke_fill=(0, 0, 0))
 
+    # save the resulting image
     img.save(f"{dannybot}\\cache\\meme_out.png")
     return
 
 # gif version
-# will only have comments on what has been modified
 def make_meme_gif(Top_Text, Bottom_Text):
 
     # iterate through every frame in the ffmpeg folder and edit them
     for frame in os.listdir(f"{dannybot}\\cache\\ffmpeg\\"):
         if '.png' in frame:
+            
+            # open image in PIL
             img = PIL.Image.open(f"{dannybot}\\cache\\ffmpeg\\{frame}")
 
+            # fixed font size calc
+            # proportionally scales the font to the size of the image, and make sure it doesn't equal 0
             imageSize = img.size
             fontSize = int(imageSize[1]/5)
             if fontSize  <= 0:
                 fontSize = 1
+
             font = ImageFont.truetype(
                 f"{dannybot}\\assets\\impactjpn.otf", fontSize)
+
+            # scale and position the text
             topTextSize = font.getsize(Top_Text)
             bottomTextSize = font.getsize(Bottom_Text)
             topTextPositionX = (imageSize[0]/2) - (topTextSize[0]/2)
             topTextPosition = (topTextPositionX, 0)
             bottomTextPositionX = (imageSize[0]/2) - (bottomTextSize[0]/2)
             bottomTextPositionY = imageSize[1] - bottomTextSize[1]
-            bottomTextPosition = (
-                bottomTextPositionX, bottomTextPositionY - 10)
-            draw = ImageDraw.Draw(img)
+            bottomTextPosition = (bottomTextPositionX, bottomTextPositionY - 10)
+
+            # FIX THE FUCKING STROKE SIZE
+            # idk why i never bothered to calculate stroke size like this
+            # it divides the size of both top and bottom text by 75 and uses that as the stroke size
+            # also we make sure it doesn't equal 0
             top_outline = int((topTextSize[0]//75))
             bottom_outline = int((bottomTextSize[0]//75))
             if top_outline <= 0:
                 top_outline = 1
             if bottom_outline <= 0:
                 bottom_outline = 1
-            draw.text(topTextPosition, Top_Text, (255, 255, 255),
-                      font=font, stroke_width=top_outline, stroke_fill=(0, 0, 0))
-            draw.text(bottomTextPosition, Bottom_Text, (255, 255, 255),
-                      font=font, stroke_width=bottom_outline, stroke_fill=(0, 0, 0))
 
-            # save each frame of the gif individually, and then pack it back into a gif
+            # draw the text
+            draw = ImageDraw.Draw(img)
+            draw.text(topTextPosition, Top_Text, (255, 255, 255), font=font,
+                    stroke_width=top_outline, stroke_fill=(0, 0, 0))
+            draw.text(bottomTextPosition, Bottom_Text, (255, 255, 255),
+                    font=font, stroke_width=bottom_outline, stroke_fill=(0, 0, 0))
+
+            # save the resulting image
             img.save(f"{dannybot}\\cache\\ffmpeg\\output\\{frame}")
             print("frame " + frame + " processed")
     repack_gif()
