246fc80b222091f2f01ec65c5c794e45c1e9742d
Danny <zestychair@gmail.com>
2023-01-20 13:22:12 -0500

    ported over dalle command

--- cogs/ai.py
+++ cogs/ai.py
@@ -8,5 +8,37 @@ class ai(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    async def dalle(self, ctx, *, prompt):
+        # rotty shit
+        images = None
+        attempt = 0
+        print("-------------------------------------")
+        print(f'Dalle command ran with prompt "{prompt}"')
+        while not images:
+            if attempt > 0:
+                print(
+                    f'Image generate request failed on attempt {attempt} for prompt "{prompt}"'
+                )
+            attempt += 1
+            images = await generate_images(prompt)
+            print(
+                f'Successfully started image generation with prompt "{prompt}" on attempt {attempt}'
+            )
+            prompt_hyphenated = prompt.replace(" ", "-")
+            collage = await make_collage(images, 3)
+            b = collage
+            collage = discord.File(
+                collage, filename=f"{prompt_hyphenated}.{DALLE_FORMAT}")
+            print("Sending image...")
+            await ctx.reply(file=collage, mention_author=True)
+            print("Caching image...")
+            with open(f"{dannybot}\\cache\\dalle.png", "wb") as f:
+                b.seek(0)
+                f.write(b.read())
+                f.close
+                print("Image Cache successful")
+                print("-------------------------------------")
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(ai(bot))
