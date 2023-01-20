f46b171d612055666e0f27c3656ad3e8d93976b7
Danny <zestychair@gmail.com>
2023-01-20 01:26:03 -0500

    added cleverbot logging

--- cogs/logging.py
+++ cogs/logging.py
@@ -16,9 +16,14 @@ class logging(commands.Cog):
     async def on_message(self, input: discord.Message):
         # make sure that we're only logging commands, rather than every message
         if input.content.startswith(self.bot.command_prefix):
-            #send all relevant information relating to command usage to our logs channel
+            # send all relevant information relating to command usage to our logs channel
             await self.bot.get_channel(logs_channel).send(f"{input.author.name} {input.author.id} issued {input.content}")
-        return
+            return
+        # log all cleverbot activity in a different manner
+        if "talk-to-dannybot" in str(input.channel.name) and not input.author.bot:
+            # send all relevant information pertaining to the current cleverbot api usage
+            await self.bot.get_channel(logs_channel).send(f"**CLEVERBOT LOGS** | *{input.author.name} {input.author.id} said:* {input.content}")
+            return
 
 
 async def setup(bot: commands.Bot):
