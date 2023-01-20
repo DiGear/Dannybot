1a44224619be746a645a320464d7b34c59fa3dd3
Danny <zestychair@gmail.com>
2023-01-20 13:36:38 -0500

    ported over fifteen.ai command
    literally cannot test it yet due to 15.ai being down

--- cogs/ai.py
+++ cogs/ai.py
@@ -3,13 +3,46 @@
 # if you can't find a variable used in this file its probably imported from here
 from config import *
 
-# this shit is dumb i wanna find a better way
+# this shit is kind of dumb i wanna find a better way
 global request_is_processing
 request_is_processing = False
 
 class ai(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command(aliases=['15', '15tts'], description="Sends AI sentences using a very real and legitimate 15.ai API.", brief="Use 15.ai to generate funny sentences")
+    async def fifteen(self, ctx, *, msg):
+        def check(msg):
+            return msg.author == ctx.author
+        global request_is_processing
+        blacklist = [1, 2]
+        if ctx.author.id in blacklist:
+            await ctx.send("You've been blacklisted from this command")
+            return
+        else:
+            if request_is_processing is True:
+                await ctx.reply(
+                    "Please allow the previous synthesis to finish.",
+                    delete_after=10,
+                    mention_author=True,
+                )
+                return
+            try:
+                await ctx.send('Which voice would you like? (It is case sensitive!)')
+                msgfunc = await self.client.wait_for("message", check=check, timeout=30)
+                requested_speaker = msgfunc.content
+            except asyncio.TimeoutError:
+                await ctx.send("Sorry, you didn't reply in time!")
+                return
+            await ctx.send("Processing... This could take a while...")
+            request_is_processing = True
+        FifteenAPI().save_to_file(f"{requested_speaker}", f"{msg}", f"{dannybot}\\cache\\15_output.wav")
+        await ctx.reply(file=discord.File(f"{dannybot}\\cache\\15_output.wav"), mention_author=True)
+        await ctx.send("This command is powered by 15.ai ^^^ https://twitter.com/fifteenai")
+        request_is_processing = False
+        return
+
 
     @commands.command()
     async def dalle(self, ctx, *, prompt):
