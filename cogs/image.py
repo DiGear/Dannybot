6e80b524059921da4c14004a345d5fc549b1ead3
Danny <zestychair@gmail.com>
2023-01-21 03:55:46 -0500

    fixed arg parsing ambiguity in meme command

--- cogs/image.py
+++ cogs/image.py
@@ -9,36 +9,18 @@ class image(commands.Cog):
         self.bot = bot
 
     @commands.command(description="Turn a provided image into an impact font meme using the syntax: toptext|bottomtext", brief="Turns an image into an impact font meme")
-    async def meme(self, ctx, context, *, meme_text: typing.Optional[str] = "ValueError"):
+    async def meme(self, ctx, *args):
         await ctx.send("Processing. Please wait... This can take a while for GIF files.", delete_after=5)
-
-        # ezogaming shit
+        
+        context = await resolve_args(ctx, args, ctx.message.attachments)
+        file_url = context[0]
+        meme_text = context[1]
         # this downloads the image to be meme'd and then configures all of the args appropriately
-        # there has to be a better way to do this
         with open(f"{dannybot}\\cache\\meme_in.png", 'wb') as f:
-            try:
-                f.write(requests.get(context).content)
-                f.close
-            except:
-                if ctx.message.attachments:
-                    meme_text_Fallback_Value = context
-                    meme_text = context + " " + meme_text
-                    if ("ValueError" in meme_text):
-                        meme_text = meme_text_Fallback_Value
-                    context = ctx.message.attachments[0].url
-                    f.write(requests.get(context).content)
-                    f.close
-                else:
-                    meme_text_Fallback_Value = context
-                    meme_text = context + " " + meme_text
-                    if ("ValueError" in meme_text):
-                        meme_text = meme_text_Fallback_Value
-                    context = await message_history_img_handler(ctx)
-                    f.write(requests.get(context).content)
+                    f.write(requests.get(file_url).content)
                     f.close
         png_path = (f"{dannybot}\\cache\\meme_in.png")
 
-        # end ezogaming shit
         # split the meme text by top and bottom and then capitalize it
         if ("|" in meme_text):
             meme_text_splitted = meme_text.split("|")
@@ -49,18 +31,18 @@ class image(commands.Cog):
             Bottom_Text = ""
 
         # determine if we need to call the standard or gif function
-        if '.gif' not in context:
+        if '.gif' not in file_url:
             make_meme(Top_Text, Bottom_Text, png_path)
         else:
             with open(f"{dannybot}\\cache\\gif.gif", 'wb') as f:
-                f.write(requests.get(context).content)
+                f.write(requests.get(file_url).content)
                 f.close
             unpack_gif(f"{dannybot}\\cache\\gif.gif")
             make_meme_gif(Top_Text, Bottom_Text)
             repack_gif()
 
         # determine if we need to send a gif or png in response
-        if '.gif' in context:
+        if '.gif' in file_url:
             with open(f"{dannybot}\\cache\\ffmpeg_out.gif", 'rb') as f:
                 await ctx.reply(file=File(f, 'meme.gif'), mention_author=True)
                 cleanup_ffmpeg()
