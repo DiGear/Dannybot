cee1ff6c22b6a45dd422f62f5146b5c7f97b0478
Danny <zestychair@gmail.com>
2023-01-25 04:11:28 -0500

    Create reactions.py

---
+++ cogs/reactions.py
@@ -0,0 +1,18 @@
+# log command usage to the console and to a specified channel
+
+# if you can't find a variable used in this file its probably imported from here
+from config import *
+
+
+class reactions(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, input: discord.Message):
+        # put reactions here
+        return
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(reactions(bot))
