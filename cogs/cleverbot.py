4c5e9497efcf07d2e570e158526d889d12f16e53
Danny <zestychair@gmail.com>
2023-01-25 01:44:24 -0500

    overhaul cleverbot to make it more "online"

--- cogs/cleverbot.py
+++ cogs/cleverbot.py
@@ -4,6 +4,7 @@
 # if you can't find a variable used in this file its probably imported from here
 from config import *
 
+nameRecall_list = ['buddy', 'pal', 'dude', 'bro']
 
 class cleverbot(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -11,8 +12,10 @@ class cleverbot(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, input: discord.Message):
+        
         # check for a valid "talk to dannybot" channel and make sure the user isn't a bot
         if "talk-to-dannybot" in str(input.channel.name) and not input.author.bot:
+            
             # this resets the conversation upon request
             if "new conversation" in input.content:
                 Cleverbot.reset()
@@ -20,8 +23,19 @@ class cleverbot(commands.Cog):
             elif "> " in input.content:
                 return
             else:
+                
+                # declare the response as a variable
+                response = Cleverbot.say(input.content)
+                
+                # remove all punctuation and capitalization to make it seem more discord user-like
+                response = str(response.translate(str.maketrans('', '', string.punctuation))).lower()
+                
+                # dumb - FDG
+                for i in range(len(nameRecall_list)):
+                    response = response.replace(nameRecall_list[i], str(input.author.name))
+                
                 # send the resulting message cleverbot api returns for our given unput
-                await input.channel.send(Cleverbot.say(input.content), reference=input)
+                await input.channel.send(response, reference=input)
                 return
 
 
