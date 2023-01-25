ef0341ed2dd4bf4914526e8151656552b9e8ace9
Danny <zestychair@gmail.com>
2023-01-25 04:06:22 -0500

    more secrets

--- cogs/secret.py
+++ cogs/secret.py
@@ -7,7 +7,16 @@ from config import *
 class secret(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+
+    @commands.command(hidden=True, aliases=["eez nuts"])
+    async def _deez_nutz_(self, ctx):
+        await ctx.reply("goteem", mention_author=True)
+
+    @commands.command(hidden=True)
+    async def bruh(self, ctx):
+        await ctx.reply("https://www.youtube.com/watch?v=beQMZ9-Ovs4", mention_author=True)
+
+       
     @commands.command(hidden=True)
     async def poopoo(self, ctx):
         pooter_file = random.choice(os.listdir(f'{dannybot}\\database\\Pooter\\'))
