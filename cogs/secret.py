7c076accae3a8221a7f9c076870085791a62b1b5
Danny <zestychair@gmail.com>
2023-01-27 14:43:18 -0500

    taur command ported

--- cogs/secret.py
+++ cogs/secret.py
@@ -29,5 +29,34 @@ class secret(commands.Cog):
         with open(f'{dannybot}\\database\\Pooter\\{pooter_file}', 'rb') as f:
             await ctx.reply(file=File(f, pooter_file))
 
+    @commands.command(hidden=True)
+    async def taur_add(self, ctx, File_Url: typing.Optional[str] = "File_Is_Attachment"):
+        whitelist = [206392667351941121, 343224184110841856] # hardcoded whitelist lol lmao
+        if not ctx.author.id in whitelist:
+            await ctx.send("You are not whitelisted for this command!")
+            return
+        else:
+            if(File_Url == "File_Is_Attachment"):
+                Link_To_File = ctx.message.attachments[0].url
+            else:
+                Link_To_File = File_Url
+            await ctx.send("Downloading...", delete_after=5)
+
+            filename = randhex(64)
+
+            # this code block writes the image data to a file
+            with open(f"{dannybot}\\database\\Taurs\\{filename}.png", 'wb') as f:
+                f.write(requests.get(Link_To_File).content)
+                f.close
+            await ctx.send("File Downloaded!", delete_after=5)
+
+    @commands.command(hidden=True)
+    async def taur(self, ctx):
+        dir = f"{dannybot}\\database\\Taurs"
+        file_name = random.choice(os.listdir(dir))
+
+        with open(f'{dir}\\{file_name}', 'rb') as f:
+            await ctx.reply(file=File(f, 'Taur.png'), mention_author=True)
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(secret(bot))
