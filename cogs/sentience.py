2d26d6fc51d84274d2ef2b5dfaa6475a5a2fc2d2
Danny <zestychair@gmail.com>
2023-01-27 02:25:03 -0500

    Update sentience.py

--- cogs/sentience.py
+++ cogs/sentience.py
@@ -14,7 +14,7 @@ class sentience(commands.Cog):
         rng = None
         
         rng = random.randint(0, dannybot_sentienceRatio)
-        if rng == dannybot_sentienceRatio and not input.author.bot and not input.guild.id == 367767486004985857:
+        if rng == dannybot_sentienceRatio and not input.author.bot and not input.guild.id == 367767486004985857 and not input.content.startswith({dannybot_prefix}):
             
             Cleverbot.reset()
         
