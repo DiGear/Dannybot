fe06c552ddca5a83652017aae412a424613eeb50
Danny <zestychair@gmail.com>
2023-02-17 03:05:45 -0500

    shit is about to get real

--- cogs/sentience.py
+++ cogs/sentience.py
@@ -6,20 +6,26 @@ from config import *
 
 class sentience(commands.Cog):
     def __init__(self, bot: commands.Bot):
+        self.conversing = False
+        self.conversing_user = None
+        self.conversing_channel = None
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, input: discord.Message):
         
-        rng = random.randint(0, dannybot_sentienceRatio) # random number generator
-        if "." in input.content and not "dannybot" in input.content:
+        rng = random.randint(0, dannybot_sentienceRatio)
+        if "." in input.content and not "dannybot" in input.content and not self.conversing and not input.content.startswith(dannybot_prefix): # if the message contains a period and is not a command or a message to dannybot
             return
         if input.author.bot: # if the author is a bot
             return
-        if rng == dannybot_sentienceRatio and not input.author.bot and not input.content.startswith(dannybot_prefix) or "dannybot" in input.content:
+        if rng == dannybot_sentienceRatio and not input.author.bot and not input.content.startswith(dannybot_prefix) or "dannybot" in input.content or self.conversing: # if the random number generator is equal to the sentience ratio and the message is not a command or a message to dannybot
         
             # declare the response as a variable and set it to the openai api
-            gpt_prompt = str(f"Respond to the following chat message. {input.author.name}: {input.content} Dannybot::")
+            if self.conversing: # if the bot is conversing with someone
+                gpt_prompt = str(f"Respond to the following chat message. {self.conversing_user}: {input.content} Dannybot::")
+            else: # if the bot is not conversing with someone
+                gpt_prompt = str(f"Respond to the following chat message. {input.author.name}: {input.content} Dannybot::")
             response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=gpt_prompt,
@@ -32,15 +38,10 @@ class sentience(commands.Cog):
             response = response['choices'][0]['text']
             
             # send the resulting message openai api returns for our given input
-            await input.channel.send(response, reference=input)
-            
-            # random image chance, send a random image from the pooter folder
-            rng = random.randint(0,4)
-            if rng == 4:
-                pooter_file = random.choice(os.listdir(f'{dannybot}\\database\\Pooter\\'))
-                with open(f'{dannybot}\\database\\Pooter\\{pooter_file}', 'rb') as f:
-                    picture = discord.File(f)
-                    await input.channel.send(file=picture, filename="dannybot")
+            if self.conversing: # if the bot is conversing with someone
+                await self.conversing_channel.send(response, reference=input)
+            else: # if the bot is not conversing with someone
+                await input.channel.send(response, reference=input)
             return
 
 async def setup(bot: commands.Bot):
