3ad6898b31184227d6413265992afaa3c4eb15e6
Danny <zestychair@gmail.com>
2023-02-19 04:51:59 -0500

    added midislap command (it probably doesn't work yet)

--- cogs/audio.py
+++ cogs/audio.py
@@ -27,5 +27,26 @@ class audio(commands.Cog):
             i.close
             v.close
 
+    @commands.command(description="Renders a midi file with a random soundfont, and sends the resulting mp3. You can also choose a specific soundfont from a list of available ones.", brief="Applies a selectable soundfont to a midi file")
+    async def midislap(self, ctx, *args):
+        # distinquish between command arguments and command file uploads
+        context = await resolve_args(ctx, args, ctx.message.attachments)
+        file_url = context[0]
+        SF2 = context[1]
+        with open(f'midislap.mid', 'wb') as midi_file:
+            midi_file.write(requests.get(file_url).content)
+            midi_file.close
+            sf2s = ["sm64.bat", "bw2.bat", "frlg.bat", "8bit.bat", "sega.bat", "touhou.bat", "mc.bat", "gba.bat", "n64.bat", "dppt.bat", "general.bat", "sgm.bat"]
+            if (SF2 == "File_Is_Attachment"):
+                SF2 = random.choice(sf2s)
+            else:
+                soundfontchoice = (SF2 + ".bat")
+            if (os.path.exists(soundfontchoice)):
+                os.system(soundfontchoice)
+                os.system('ffmpeg -y -i slapped.wav -vn -ar 44100  -ac 2 -b:a 96k slappednorm.mp3')
+                with open(f'slappednorm.mp3', 'rb') as f:
+                    await ctx.reply('Midislapped with ' + str(soundfontchoice).replace('.bat', '.sf2'), file=File(f, 'midislap.mp3'))
+                    f.close
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(audio(bot))
