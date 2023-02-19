b44b7acde69044eae1606fbb395b372adb24b52f
Danny <zestychair@gmail.com>
2023-02-19 14:59:51 -0500

    EVERYTHING IS PORTED OVER

--- cogs/server.py
+++ cogs/server.py
@@ -84,6 +84,13 @@ class server(commands.Cog):
         with open(f'{dir}\\{file_name}', 'rb') as f:
             await ctx.reply(file=File(f, 'glass.png'), mention_author=True)
             
+    @commands.command(description="Send a picture of a burger.", brief="Send a picture of a burger")
+    async def burger(self, ctx):
+        dir = f"{dannybot}\\database\\Burger"
+        file_name = random.choice(os.listdir(dir))
+        with open(f'{dir}\\{file_name}', 'rb') as f:
+            await ctx.reply(file=File(f, 'burger.png'), mention_author=True)
+            
     @commands.command(description="Display file counts for key directories in Dannybot", brief="Display file counts for key directories in Dannybot")
     async def db(self, ctx):
         embed = discord.Embed(title="Dannybot File Totals", color=0xf77e9a)
