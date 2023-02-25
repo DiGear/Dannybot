f719761f1a728093474c7ce9a1500bcb17159258
Danny <zestychair@gmail.com>
2023-02-24 23:24:26 -0500

    make the db command worse

--- cogs/server.py
+++ cogs/server.py
@@ -100,20 +100,20 @@ class server(commands.Cog):
             
     @commands.command(description="Display file counts for key directories in Dannybot", brief="Display file counts for key directories in Dannybot")
     async def db(self, ctx):
+
         embed = discord.Embed(title="Dannybot File Totals", color=0xf77e9a)
-        #embed.add_field(name="Pizzi AI Image Files:", value=fileCount(f"{dannybot}\\database\\Dooter"), inline=True)
-        embed.add_field(name="Pooter Files:", value=fileCount(f"{dannybot}\\database\\Pooter"), inline=True)
-        embed.add_field(name="Leffrey Files:", value=fileCount(f"{dannybot}\\database\\Leffrey"), inline=True)
-        embed.add_field(name="Femboy Files:", value=fileCount(f"{dannybot}\\database\\Femboy"), inline=True)
-        embed.add_field(name="Fanboy Files:", value=fileCount(f"{dannybot}\\database\\Fanboy"), inline=True)
-        embed.add_field(name="Video Files:", value=fileCount(VideosPath), inline=True)
-        embed.add_field(name="Image Files:", value=fileCount(PicturesPath), inline=True)
-        embed.add_field(name="GIF Files:", value=fileCount(GifsPath), inline=True)
-        embed.add_field(name="Glass Cup Images:", value=fileCount(f"{dannybot}\\database\\Glasscup"), inline=True)
-        embed.add_field(name="Plastic Cup Images:", value=fileCount(f"{dannybot}\\database\\Plasticcup"), inline=True)
-        embed.add_field(name="Burger Files:", value=fileCount(f"{dannybot}\\database\\Burger"), inline=True)
-        embed.add_field(name="Nekopara Files:", value=fileCount(NekoparaPath), inline=True)
-        embed.add_field(name="Animal Girl Images:", value=fileCount(f"{dannybot}\\database\\Mimi"), inline=True)
+        embed.add_field(name="Pooter Files:", value=str(str(fileCount(f"{dannybot}\\database\\Pooter")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Pooter"))), inline=True)
+        embed.add_field(name="Leffrey Files:", value=str(str(fileCount(f"{dannybot}\\database\\Leffrey")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Leffrey"))), inline=True)
+        embed.add_field(name="Femboy Files:", value=str(str(fileCount(f"{dannybot}\\database\\Femboy")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Femboy"))), inline=True)
+        embed.add_field(name="Fanboy Files:", value=str(str(fileCount(f"{dannybot}\\database\\Fanboy")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Fanboy"))), inline=True)
+        embed.add_field(name="Glass Cup Images:", value=str(str(fileCount(f"{dannybot}\\database\\Glasscup")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Glasscup"))), inline=True)
+        embed.add_field(name="Plastic Cup Images:", value=str(str(fileCount(f"{dannybot}\\database\\Plasticcup")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Plasticcup"))), inline=True)
+        embed.add_field(name="Burger Files:", value=str(str(fileCount(f"{dannybot}\\database\\Burger")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Burger"))), inline=True)
+        embed.add_field(name="Nekopara Files:", value=str(str(fileCount(NekoparaPath)) + " files \n" + str(fileSize(NekoparaPath))), inline=True)
+        embed.add_field(name="Animal Girl Images:", value=str(str(fileCount(f"{dannybot}\\database\\Mimi")) + " files \n" + str(fileSize(f"{dannybot}\\database\\Mimi"))), inline=True)
+        embed.add_field(name="Video Files:", value=str(str(fileCount(VideosPath)) + " files \n" + str(fileSize(VideosPath))), inline=True)
+        embed.add_field(name="Image Files:", value=str(str(fileCount(PicturesPath)) + " files \n" + str(fileSize(PicturesPath))), inline=True)
+        embed.add_field(name="GIF Files:", value=str(str(fileCount(GifsPath)) + " files \n" + str(fileSize(GifsPath))), inline=True)
 
         await ctx.reply(embed=embed, mention_author=True)
 
