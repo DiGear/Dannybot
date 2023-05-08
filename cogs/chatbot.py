fdacd6fc704ecea5bb66af1c6840fc23bcb4fd89
Danny <zestychair@gmail.com>
2023-05-07 21:50:25 -0400

    chatbot variable rename

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -13,10 +13,10 @@ class sentience(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.sysmsg = "You are Dannybot, a discord bot created by a man named FDG. You can chat with people and also issue commands for them. Your pronouns are He/Him, and you are 3 years old. Do not refer to yourself as an 'AI Language Model'."
-        self.memorylength = 15
-        self.messagearray = [{"role": "system", "content": self.sysmsg}]
-        self.arrayindex = 0
-        self.allowedInVC = True
+        self.memory_length = 15
+        self.message_array = [{"role": "system", "content": self.sysmsg}]
+        self.array_index = 0
+        self.allowed_in_voice_channel = True
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -31,21 +31,21 @@ class sentience(commands.Cog):
 
         if not message.author.bot and ("dannybot" in content.lower() or content.lower().endswith("dannybot")):
             content = content.replace('dannybot', '')
-            self.messagearray.append({"role": "user", "content": f"{message.author.name} said: {content}"})
-            self.arrayindex += 1
+            self.message_array.append({"role": "user", "content": f"{message.author.name} said: {content}"})
+            self.array_index += 1
 
-            if self.arrayindex > self.memorylength:
-                self.arrayindex = 0
-                self.messagearray = [{"role": "system", "content": self.sysmsg}]
+            if self.array_index > self.memory_length:
+                self.array_index = 0
+                self.message_array = [{"role": "system", "content": self.sysmsg}]
 
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=self.messagearray
+                messages=self.message_array
             )
 
             print(f"{message.author.name} said: {content}")
-            responsearray = response.choices[0].message.content
-            self.arrayindex += 1
+            response_array = response.choices[0].message.content
+            self.array_index += 1
 
             if message.author.voice and message.author.voice.channel:
                 voice_state = message.guild.voice_client
@@ -54,25 +54,25 @@ class sentience(commands.Cog):
                 if voice_state and voice_state.is_playing():
                     pass
                 else:
-                    if self.allowedInVC:
+                    if self.allowed_in_voice_channel:
                         voice_client = voice_state or await voice_channel.connect()
 
                 engine = pyttsx3.init()
                 engine.setProperty('rate', random.randint(125, 175))
                 engine.setProperty('volume', 0.75)
                 output_file = 'ChatGPT.wav'
-                text = responsearray
+                text = response_array
                 engine.save_to_file(text, output_file)
                 engine.runAndWait()
 
-                await message.channel.send(responsearray, reference=message)
+                await message.channel.send(response_array, reference=message)
 
                 audio_source = discord.FFmpegPCMAudio('ChatGPT.wav')
                 voice_client.play(audio_source)
             else:
-                await message.channel.send(responsearray, reference=message)
+                await message.channel.send(response_array, reference=message)
 
-            self.messagearray.append({"role": "assistant", "content": responsearray})
+            self.message_array.append({"role": "assistant", "content": response_array})
 
     @commands.command(description="Interact with GPT3.5 using Dannybot.", brief="Get AI-generated text based on provided prompts")
     async def gptinstruct(self, ctx, *, flags: CustomGPT):
