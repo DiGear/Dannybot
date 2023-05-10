103e2e5b9f0372ed9e8b4045119479a9abad521a
Danny <zestychair@gmail.com>
2023-05-09 21:34:38 -0400

    Update audio.py

--- cogs/audio.py
+++ cogs/audio.py
@@ -7,22 +7,6 @@ class audio(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         
-    #32 bit only lmao
-    '''
-    @commands.command(description="Touhou Yukkuri text-to-speech.", brief="Touhou TTS")
-    async def yukkuri(self, ctx, *args):
-        aq = aquestalk.load('f2')
-        wav = aq.synthe(str(args))
-        with wave.open("yukkuri.wav", "wb") as wav_file:
-            wav_file.setnchannels(1)  # Mono
-            wav_file.setsampwidth(2)  # 16-bit
-            wav_file.setframerate(44100)  # 44.1 kHz
-            wav_file.writeframesraw(wav)
-            with open(f'yukkuri.wav', 'rb') as i:
-                await ctx.reply(file=File(i, 'tts.wav'))
-                i.close
-        '''
-        
     @commands.command()
     async def join(self, ctx):
         channel = ctx.author.voice.channel
@@ -39,62 +23,77 @@ class audio(commands.Cog):
     async def acapella(self, ctx, *args):
         cmd_info = await resolve_args(ctx, args, ctx.message.attachments)
         file_url = cmd_info[0]
-        with open(f'{UltimateVocalRemover}\\audio.mp3', 'wb') as f:
+
+        audio_path = f'{UltimateVocalRemover}\\audio.mp3'
+        instruments_path = f'{UltimateVocalRemover}\\audio_Instruments.mp3'
+        vocals_path = f'{UltimateVocalRemover}\\audio_Vocals.mp3'
+
+        with open(audio_path, 'wb') as f:
             f.write(requests.get(file_url).content)
-            f.close
+
         await ctx.send("Splitting. Please wait...")
-        os.system(f"cd {UltimateVocalRemover}")
-        os.chdir(f"{UltimateVocalRemover}")
-        os.system('python inference.py --input audio.mp3 --tta --gpu 0')
-        os.system("ffmpeg -i audio_Instruments.wav -vn -ar 44100 -ac 2 -b:a 192k audio_Instruments.mp3 -y")
-        os.system("ffmpeg -i audio_Vocals.wav -vn -ar 44100 -ac 2 -b:a 192k audio_Vocals.mp3 -y")
-        with open(f'audio_Instruments.mp3', 'rb') as i, open(f'audio_Vocals.mp3', 'rb') as v:
+
+        os.chdir(UltimateVocalRemover)
+        os.system(f'python inference.py --input {audio_path} --tta --gpu 0')
+        os.system(f'ffmpeg -i audio_Instruments.wav -vn -ar 44100 -ac 2 -b:a 192k {instruments_path} -y')
+        os.system(f'ffmpeg -i audio_Vocals.wav -vn -ar 44100 -ac 2 -b:a 192k {vocals_path} -y')
+
+        with open(instruments_path, 'rb') as i, open(vocals_path, 'rb') as v:
             await ctx.reply(file=File(i, 'Inst.mp3'))
             await ctx.reply(file=File(v, 'Vocal.mp3'))
-            i.close
-            v.close
-            
-    @commands.command(description="Flips a midi file upside down, relative to middle C (C5).", brief="Flips a midi file upside down")
+                
+    @commands.command(description="Flips a MIDI file upside down, relative to middle C (C5).", brief="Flips a MIDI file upside down")
     async def midiflip(self, ctx, *args):
         cmd_info = await resolve_args(ctx, args, ctx.message.attachments, "midi")
         file_url = cmd_info[0]
+
         with open(f'{dannybot}\\cache\\midiflip.mid', 'wb') as f:
             f.write(requests.get(file_url).content)
-            f.close
+
         os.system(f"python NegativeHarmonizer.py {dannybot}\\cache\\midiflip.mid --tonic 60 --ignore 9 --adjust-octaves")
-        os.system(f"fluidsynth -ni {dannybot}\\assets\\SF2\\general.sf2 {dannybot}\\cache\\midiflip_negative.mid -F {dannybot}\\cache\\midislap_{ctx.message.id}.wav -r 44100")
-        os.system(f"ffmpeg-normalize {dannybot}\\cache\\midislap_{ctx.message.id}.wav -o {dannybot}\\cache\\midislap_{ctx.message.id}.ogg -c:a libopus -b:a 64k --keep-loudness-range-target -f")
-        with open(f'{dannybot}\\cache\\midiflip_negative.mid', 'rb') as i, open(f'{dannybot}\\cache\\midislap_{ctx.message.id}.ogg', 'rb') as f:
+
+        midi_output_path = f'{dannybot}\\cache\\midiflip_negative.mid'
+        os.system(f"fluidsynth -ni {dannybot}\\assets\\SF2\\general.sf2 {midi_output_path} -F {dannybot}\\cache\\midislap_{ctx.message.id}.wav -r 44100")
+
+        ogg_output_path = f'{dannybot}\\cache\\midislap_{ctx.message.id}.ogg'
+        os.system(f"ffmpeg-normalize {dannybot}\\cache\\midislap_{ctx.message.id}.wav -o {ogg_output_path} -c:a libopus -b:a 64k --keep-loudness-range-target -f")
+
+        with open(f'{dannybot}\\cache\\midiflip_negative.mid', 'rb') as i, open(ogg_output_path, 'rb') as f:
             await ctx.reply(file=File(i, 'flipped.mid'))
             await ctx.reply(f"Audio preview:", file=File(f, 'midislap.ogg'))
-            f.close
-            i.close
+
 
     # i need to find a nice way to implement a viewable list of soundfonts
-    @commands.command(alisases=['nathans'], description="Renders a midi file with a random soundfont, and sends the resulting audio. You can also choose a specific soundfont from a list of available ones.", brief="Applies a selectable soundfont to a midi file")
+    @commands.command(aliases=['nathans'], description="Renders a MIDI file with a random soundfont and sends the resulting audio. You can also choose a specific soundfont from a list of available ones.", brief="Applies a selectable soundfont to a MIDI file")
     async def midislap(self, ctx, *args):
         sf2s = os.listdir(f"{dannybot}\\assets\\SF2\\")
         context = await resolve_args(ctx, args, ctx.message.attachments, "midi")
+        
         if not args and not ctx.message.attachments:
-            await ctx.reply("The list of selectable soundfonts is as follows:\n" + str(listgen(f"{dannybot}\\assets\\SF2\\")).replace(".sf2", ""))
+            soundfonts_list = "\n".join(sf2.replace(".sf2", "") for sf2 in sf2s)
+            await ctx.reply("The list of selectable soundfonts is as follows:\n" + soundfonts_list)
             return
+
         file_url = context[0]
-        if not context[1]:
-            SF2 = random.choice(sf2s)
-        else:
-            SF2 = context[1] + ".sf2"
+        SF2 = context[1] + ".sf2" if context[1] else random.choice(sf2s)
+
         with open(f'{dannybot}\\cache\\midislap.mid', 'wb') as midi_file:
             midi_file.write(requests.get(file_url).content)
-            midi_file.close
+
         if SF2 not in sf2s:
             await ctx.reply("Please choose a valid soundfont!")
-        else:
-            await ctx.send("Generating... Use 'd.midislap' on it's own to see a list of selectable soundfonts...", delete_after=10)
-            os.system(f"fluidsynth -ni {dannybot}\\assets\\SF2\\{SF2} {dannybot}\\cache\\midislap.mid -F {dannybot}\\cache\\midislap_{ctx.message.id}.wav -r 44100")
-            os.system(f"ffmpeg-normalize {dannybot}\\cache\\midislap_{ctx.message.id}.wav -o {dannybot}\\cache\\midislap_{ctx.message.id}.ogg -c:a libopus -b:a 64k -f")
-            with open(f'{dannybot}\\cache\\midislap_{ctx.message.id}.ogg', 'rb') as f:
-                await ctx.reply(f"Midislapped with {SF2}:", file=File(f, 'midislap.ogg'))
-                f.close
+            return
+
+        await ctx.send("Generating... Use 'd.midislap' on its own to see a list of selectable soundfonts...", delete_after=10)
+
+        midi_output_path = f'{dannybot}\\cache\\midislap_{ctx.message.id}.wav'
+        os.system(f"fluidsynth -ni {dannybot}\\assets\\SF2\\{SF2} {dannybot}\\cache\\midislap.mid -F {midi_output_path} -r 44100")
+
+        ogg_output_path = f'{dannybot}\\cache\\midislap_{ctx.message.id}.ogg'
+        os.system(f"ffmpeg-normalize {midi_output_path} -o {ogg_output_path} -c:a libopus -b:a 64k -f")
+
+        with open(ogg_output_path, 'rb') as f:
+            await ctx.reply(f"Midislapped with {SF2}:", file=File(f, 'midislap.ogg'))
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(audio(bot))
