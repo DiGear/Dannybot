c8d0e8ae38ac9145567f4e88580c8217f5ad12aa
Danny <zestychair@gmail.com>
2023-05-09 21:17:47 -0400

    Update errors.py

--- cogs/errors.py
+++ cogs/errors.py
@@ -10,40 +10,40 @@ class errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        
-        # ignore "intentional errors": errors that get caught mid-function
+        # Ignore "intentional errors": errors that get caught mid-function
         if hasattr(ctx.command, "on_error"):
             return
 
-        # set specific errors to be ignored, such as the commands.CommandNotFound error
+        # Set specific errors to be ignored, such as commands.CommandNotFound
         ignored = (commands.CommandNotFound,)
 
         error = getattr(error, "original", error)
 
-        # ignore errors in the ignore list
+        # Ignore errors in the ignore list
         if isinstance(error, ignored):
             return
 
-        # command is disabled
+        # Command is disabled
         if isinstance(error, commands.DisabledCommand):
             await ctx.reply(f"{ctx.command} has been disabled.")
 
-        # command requires developer permissions
+        # Command requires developer permissions
         elif isinstance(error, commands.errors.NotOwner):
-            await ctx.reply(f"{ctx.command} is reserved for Dannybot developers. <:trollface:855665633509507092>") # trollface emote is from dannybots mother server, will need to replace emote or remove it
+            await ctx.reply(f"{ctx.command} is reserved for Dannybot developers. <:trollface:855665633509507092>")  # Replace or remove the trollface emote
 
-        # command was sent in a DM
+        # Command was sent in a DM
         elif isinstance(error, commands.NoPrivateMessage):
-            try: # send the error response in the DM, if possible
+            try:
                 await ctx.author.send(f"{ctx.command} can not be used in Private Messages.")
             except discord.HTTPException:
                 pass
+
         else:
-            # this is a catch-all for any other types of errors, they will be sent in chat and the console for debugging purposes
-            await ctx.reply(f"An undefined error has occured.\n```\n{error.__traceback__}\n{error}```\nIf you are seeing this, ping FDG for assistance.")
+            # Handle other types of errors
+            error_message = f"An undefined error has occurred.\n```py\n{type(error).__name__}: {error}\n```"
+            await ctx.reply(error_message)
             print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(errors(bot))
