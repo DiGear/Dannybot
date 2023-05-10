30267862a92321ae8aa7e28ca8a8e55c7e24bd60
Danny <zestychair@gmail.com>
2023-05-10 00:17:55 -0400

    Update ai.py

--- cogs/ai.py
+++ cogs/ai.py
@@ -13,46 +13,51 @@ class ai(commands.Cog):
 
     @commands.command(aliases=['gpt'], description="Interact with GPT3 using Dannybot.", brief="Get AI generated text based on provided prompts")
     async def write(self, ctx, *, prompt):
-        gpt_prompt = str(f"write me {prompt}")
-        print(gpt_prompt)
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=gpt_prompt,
-            temperature=random.uniform(0., 1.0),
-            max_tokens=256,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
-        await ctx.reply(response['choices'][0]['text'], mention_author=True)
+        try:        
+            gpt_prompt = str(f"write me {prompt}")
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=gpt_prompt,
+                temperature=random.uniform(0., 1.0),
+                max_tokens=256,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+            )
+            await ctx.reply(response['choices'][0]['text'], mention_author=True)
+        except Exception as e:
+            await ctx.send(f"An error occurred while generating the text: {str(e)}")
         
     @commands.command(description="Interact with GPT3 using Dannybot.", brief="Get AI generated text based on provided prompts")
     async def gpttemp(self, ctx, temp: typing.Optional[float] = 0.7, *, prompt):
-        gpt_prompt = str(prompt)
-        print(gpt_prompt)
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=gpt_prompt,
-            temperature=temp,
-            max_tokens=256,
-            top_p=1.0,
-            frequency_penalty=1.0,
-            presence_penalty=0.0
-        )
-        await ctx.reply(response['choices'][0]['text'], mention_author=True)
+        try:
+            gpt_prompt = str(prompt)
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=gpt_prompt,
+                temperature=temp,
+                max_tokens=256,
+                top_p=1.0,
+                frequency_penalty=1.0,
+                presence_penalty=0.0
+            )
+            await ctx.reply(response['choices'][0]['text'], mention_author=True)
+        except Exception as e:
+            await ctx.send(f"An error occurred while generating the text: {str(e)}")
 
     @commands.command(aliases=['upscale'], description="Locally run waifu2x using speed-optimized settings and send the results.", brief="Upscale images using waifu2x")
     async def waifu(self, ctx, *args):
         cmd_info = await resolve_args(ctx, args, ctx.message.attachments)
         Link_To_File = cmd_info[0]
         await ctx.send("Upscaling. Please wait...")
-        with open(f'{dannybot}\\cache\\w2x_in.png', 'wb') as f:
-            f.write(requests.get(Link_To_File).content)
-            f.close
-        os.system(f"{Waifu2x} -i {dannybot}\\cache\\w2x_in.png -o {dannybot}\\cache\\w2x_out.png -m noise_scale --scale_ratio 2 --noise_level 2 -x")
-        with open(f'{dannybot}\\cache\\w2x_out.png', 'rb') as f:
-            await ctx.reply(file=File(f, 'waifu2x.png'))
-        f.close
+        try:
+            with open(f'{dannybot}\\cache\\w2x_in.png', 'wb') as f:
+                f.write(requests.get(Link_To_File).content)
+            os.system(f"{Waifu2x} -i {dannybot}\\cache\\w2x_in.png -o {dannybot}\\cache\\w2x_out.png -m noise_scale --scale_ratio 2 --noise_level 2 -x")
+            with open(f'{dannybot}\\cache\\w2x_out.png', 'rb') as f:
+                await ctx.reply(file=File(f, 'waifu2x.png'))
+        except Exception as e:
+            await ctx.send(f"An error occurred while upscaling the image: {str(e)}")
 
     @commands.command(aliases=['15', '15tts'], description="Sends AI sentences using a very real and legitimate 15.ai API.", brief="Use 15.ai to generate funny sentences")
     async def fifteen(self, ctx, *, msg):
@@ -89,35 +94,66 @@ class ai(commands.Cog):
     @commands.command(aliases=['quote'], description="Sends AI generated quotes using the inspirobot API.", brief="Get AI generated inspirational posters")
     async def inspire(self, ctx):
         link = "http://inspirobot.me/api?generate=true"
-        f = requests.get(link)
-        File_Url = f.text
-        img = PIL.Image.open(requests.get(File_Url, stream=True).raw)
-        img.save(f"{dannybot}\\cache\\quote.jpg", "JPEG")
-        f = discord.File(f"{dannybot}\\cache\\quote.jpg", filename="quote.jpg")
-        embed = discord.Embed(color=0xffc7ed)
-        embed.set_image(url="attachment://quote.jpg")
-        embed.set_footer(text="Powered by https://inspirobot.me/")
-        await ctx.reply(file=f, embed=embed, mention_author=True)
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(link) as response:
+                    if response.status == 200:
+                        File_Url = await response.text()
+                        async with session.get(File_Url) as image_response:
+                            if image_response.status == 200:
+                                image_data = await image_response.read()
+                                with open(f"{dannybot}\\cache\\quote.jpg", "wb") as f:
+                                    f.write(image_data)
+
+                                f = discord.File(f"{dannybot}\\cache\\quote.jpg", filename="quote.jpg")
+                                embed = discord.Embed(color=0xffc7ed)
+                                embed.set_image(url="attachment://quote.jpg")
+                                embed.set_footer(text="Powered by https://inspirobot.me/")
+                                await ctx.reply(file=f, embed=embed, mention_author=True)
+                            else:
+                                await ctx.reply("Failed to retrieve the image from InspiroBot.", mention_author=True)
+                    else:
+                        await ctx.reply("Failed to generate an inspirational quote from InspiroBot.", mention_author=True)
+        except aiohttp.ClientError:
+            await ctx.reply("An error occurred while making the request to InspiroBot.", mention_author=True)
+        except OSError:
+            await ctx.reply("An error occurred while saving the image.", mention_author=True)
 
     @commands.command(aliases=['pngify', 'transparent'], description="Runs the provided image through a (free) API call to remove.bg, to make the image transparent.", brief="Remove the background from an image using AI")
     async def removebg(self, ctx, *args):
         cmd_info = await resolve_args(ctx, args, ctx.message.attachments)
         Link_To_File = cmd_info[0]
         await ctx.send("Removing background. Please wait...", delete_after=5)
-        with open(f'{dannybot}\\cache\\removebgtemp.png', 'wb') as f:
-            f.write(requests.get(Link_To_File).content)
-            f.close
-        response = requests.post("https://api.remove.bg/v1.0/removebg", data={"image_url": str(Link_To_File), "size": "auto"}, headers={"X-Api-Key": os.getenv("REMOVEBG_KEY")},)
-        if response.status_code == requests.codes.ok:
-            with open(f'{dannybot}\\cache\\removebg.png', 'wb') as out:
-                out.write(response.content)
-                f.close
-            with open(f'{dannybot}\\cache\\removebg.png', 'rb') as f:
-                await ctx.reply(file=File(f, 'removed.png'), mention_author=True)
-                f.close
-        else:
-            await ctx.reply("Processing of the image failed. This is most likely because no background was detected.", mention_author=True)
-            print(response.content)
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(Link_To_File) as response:
+                image_data = await response.read()
+                
+                with open(f'{dannybot}\\cache\\removebgtemp.png', 'wb') as f:
+                    f.write(image_data)
+                
+                headers = {
+                    "X-Api-Key": os.getenv("REMOVEBG_KEY"),
+                }
+                
+                async with session.post(
+                    "https://api.remove.bg/v1.0/removebg",
+                    data={"image_file": image_data, "size": "auto"},
+                    headers=headers,
+                ) as response:
+                    if response.status == 200:
+                        result_data = await response.read()
+                        
+                        with open(f'{dannybot}\\cache\\removebg.png', 'wb') as f:
+                            f.write(result_data)
+                        
+                        with open(f'{dannybot}\\cache\\removebg.png', 'rb') as f:
+                            await ctx.reply(file=File(f, 'removed.png'), mention_author=True)
+                    
+                    else:
+                        await ctx.reply("Processing of the image failed. This is most likely because no background was detected.", mention_author=True)
+                        print(await response.text())
 
     @commands.command(description="Uses the craiyon API to send user prompts and return AI generated output.", brief="Use craiyon to create AI generated images")
     async def dalle(self, ctx, *, prompt):
