581ddb61b28de6154ef68257395652a0ae199456
Danny <zestychair@gmail.com>
2023-05-15 02:41:28 -0400

    Update reactions.py

--- disabled cogs/reactions.py
+++ disabled cogs/reactions.py
@@ -1,20 +1,11 @@
-# log command usage to the console and to a specified channel
-
 # if you can't find a variable used in this file its probably imported from here
 from config import *
-
+import discord
 
 class reactions(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        if message.content.lower().startswith("balls in"):
-            await message.channel.send("https://cdn.discordapp.com/attachments/947963019319709777/1080006889468862555/maxresdefault.png", reference=message)
-        elif message.content.lower().startswith("balls out"):
-            await message.channel.send("https://cdn.discordapp.com/attachments/947963019319709777/1080011687979122779/latest.png", reference=message)
-        reactions = {
+        self.reactions = {
             '🐍': ['python', 'py'],
             '♨️': ['java', 'jar'],
             '💎': ['ruby', 'rb'],
@@ -22,11 +13,18 @@ class reactions(commands.Cog):
             '✋': ['yay'],
             '💥': ['yay']
         }
-        for emoji, keywords in reactions.items():
-            for keyword in keywords:
-                if keyword in message.content.lower():
-                    await message.add_reaction(emoji)
-        return
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        content = message.content.lower()
+        if content.startswith("balls in"):
+            await message.channel.send("https://cdn.discordapp.com/attachments/947963019319709777/1080006889468862555/maxresdefault.png", reference=message)
+        elif content.startswith("balls out"):
+            await message.channel.send("https://cdn.discordapp.com/attachments/947963019319709777/1080011687979122779/latest.png", reference=message)
+
+        for emoji, keywords in self.reactions.items():
+            if any(keyword in content for keyword in keywords):
+                await message.add_reaction(emoji)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(reactions(bot))
