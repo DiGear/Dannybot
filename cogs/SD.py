234ee9ef0f3f26bce31eee27c6d28e6d1ca9d503
Danny <zestychair@gmail.com>
2023-05-22 02:38:33 -0400

    brap

--- cogs/SD.py
+++ cogs/SD.py
@@ -1,15 +1,11 @@
-# obligatory moderation commands - FDG
-
-# this shit is still empty lol
-
 # if you can't find a variable used in this file its probably imported from here
 from config import *
 logger = logging.getLogger(__name__)
 
 
-class moderation(commands.Cog):
+class sd(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(moderation(bot))
+    await bot.add_cog(sd(bot))
