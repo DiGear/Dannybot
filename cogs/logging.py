ad2226b07d35876af53482ce0811eb960735685c
Danny <zestychair@gmail.com>
2023-09-04 19:39:10 -0400

    make logging a bit less stupid

--- cogs/logging.py
+++ cogs/logging.py
@@ -11,13 +11,10 @@ class logging(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        # Check if the message starts with any of the bot's prefixes
-        for prefix in dannybot_prefixes:
-            if message.content.startswith(prefix):
-                # Send command usage information to the logs channel
-                log_message = f"{message.author.global_name} ({message.author.id}) issued: {message.content}"
-                await self.bot.get_channel(logs_channel).send(log_message)
-            break
+        log_channel = self.bot.get_channel(logs_channel)
+        if any(message.content.startswith(prefix) for prefix in dannybot_prefixes):
+            log_message = f"{message.author.global_name} ({message.author.id}) issued: {message.content}"
+            await log_channel.send(log_message)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(logging(bot))
