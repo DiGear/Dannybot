8da641c4139935e1850c5567be0357854d8460eb
Danny <zestychair@gmail.com>
2023-09-05 03:18:50 -0400

    dictionary

--- cogs/server.py
+++ cogs/server.py
@@ -53,41 +53,31 @@ class server(commands.Cog):
         
     @commands.hybrid_command(name="database", aliases=['db', 'databases', 'dbs'], description="Display file counts for key directories in Dannybot", brief="Display file counts for key directories in Dannybot")
     async def db(self, ctx):
-        # Define directory paths
-        pooter_path = f"{dannybot}\\database\\Pooter"
-        danny_path = "I:\\Danny Infinitum"
-        leffrey_path = f"{dannybot}\\database\\Leffrey"
-        femboy_path = f"{dannybot}\\database\\Femboy"
-        fanboy_path = f"{dannybot}\\database\\Fanboy"
-        glasscup_path = f"{dannybot}\\database\\Glasscup"
-        plasticcup_path = f"{dannybot}\\database\\Plasticcup"
-        burger_path = f"{dannybot}\\database\\Burger"
-        nekopara_path = NekoparaPath
-        animalgirl_path = f"{dannybot}\\database\\Mimi"
-        video_path = VideosPath
-        image_path = PicturesPath
-        gif_path = GifsPath
+        # Define directory paths in a dictionary
+        directory_paths = {
+            'Pooter Files:': f"{dannybot}\\database\\Pooter",
+            'Danny Files:': "I:\\Danny Infinitum",
+            'Leffrey Files:': f"{dannybot}\\database\\Leffrey",
+            'Femboy Files:': f"{dannybot}\\database\\Femboy",
+            'Fanboy Files:': f"{dannybot}\\database\\Fanboy",
+            'Glass Cup Images:': f"{dannybot}\\database\\Glasscup",
+            'Plastic Cup Images:': f"{dannybot}\\database\\Plasticcup",
+            'Burger Files:': f"{dannybot}\\database\\Burger",
+            'Nekopara Files:': NekoparaPath,
+            'Animal Girl Images:': f"{dannybot}\\database\\Mimi",
+            'Video Files:': VideosPath,
+            'Image Files:': PicturesPath,
+            'GIF Files:': GifsPath
+        }
 
         # Create the embed
         embed = discord.Embed(title="Dannybot File Totals", color=0xf77e9a)
 
         # Add fields to the embed
-        embed.add_field(name="Pooter Files:", value=f"{fileCount(pooter_path)} files\n{fileSize(pooter_path)}")
-        embed.add_field(name="Danny Files:", value=f"{fileCount(danny_path)} files\n{fileSize(danny_path)}")
-        embed.add_field(name="Leffrey Files:", value=f"{fileCount(leffrey_path)} files\n{fileSize(leffrey_path)}")
-        embed.add_field(name="Femboy Files:", value=f"{fileCount(femboy_path)} files\n{fileSize(femboy_path)}")
-        embed.add_field(name="Fanboy Files:", value=f"{fileCount(fanboy_path)} files\n{fileSize(fanboy_path)}")
-        embed.add_field(name="Glass Cup Images:", value=f"{fileCount(glasscup_path)} files\n{fileSize(glasscup_path)}")
-        embed.add_field(name="Plastic Cup Images:", value=f"{fileCount(plasticcup_path)} files\n{fileSize(plasticcup_path)}")
-        embed.add_field(name="Burger Files:", value=f"{fileCount(burger_path)} files\n{fileSize(burger_path)}")
-        embed.add_field(name="Nekopara Files:", value=f"{fileCount(nekopara_path)} files\n{fileSize(nekopara_path)}")
-        embed.add_field(name="Animal Girl Images:", value=f"{fileCount(animalgirl_path)} files\n{fileSize(animalgirl_path)}")
-        embed.add_field(name="Video Files:", value=f"{fileCount(video_path)} files\n{fileSize(video_path)}")
-        embed.add_field(name="Image Files:", value=f"{fileCount(image_path)} files\n{fileSize(image_path)}")
-        embed.add_field(name="GIF Files:", value=f"{fileCount(gif_path)} files\n{fileSize(gif_path)}")
+        for name, path in directory_paths.items():
+            embed.add_field(name=name, value=f"{fileCount(path)} files\n{fileSize(path)}")
 
         await ctx.reply(embed=embed, mention_author=True)
-
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(server(bot))
