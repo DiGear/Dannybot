a411e8121b0d38950d45c95dd69e054311af06e1
Danny <zestychair@gmail.com>
2023-09-05 03:12:26 -0400

    make this a dict

--- cogs/user.py
+++ cogs/user.py
@@ -29,19 +29,25 @@ class user(commands.Cog):
     @commands.hybrid_command(name='info', description="Grab the users account information, and then send it in an embed.", brief="Display provided users information")
     async def info(self, ctx: commands.Context, member: discord.Member = None):
         member = member or ctx.author
-        embed = discord.Embed(title="User Information", color=member.color)
-        embed.set_thumbnail(url=member.avatar.url)
-        embed.add_field(name="Display name:", value=f"{member.global_name}", inline=False)
-        embed.add_field(name="Username:", value=f"{member.name}", inline=False)
-        embed.add_field(name="Server Name:", value=str(member.display_name), inline=False)
-        embed.add_field(name="ID:", value=str(member.id), inline=False)
-        embed.add_field(name="Status:", value=str(member.raw_status))
-        embed.add_field(name="Is active on mobile:", value=str(member.is_on_mobile()))
-        embed.add_field(name="Is Bot account:", value=str(member.bot))
-        embed.add_field(name="Account Creation Date:", value=member.created_at.strftime("%#d, %B, %Y at %I:%M %p UTC"), inline=False)
-        embed.add_field(name="Server Join Date:", value=member.joined_at.strftime("%#d, %B, %Y at %I:%M %p UTC"), inline=False)
-        embed.set_footer(text=f"Command ran on {member.guild}.")
-        await ctx.reply(embed=embed, mention_author=True)
-
+        date_format = "%#d, %B, %Y at %I:%M %p UTC"
+        embed_info = {
+            "title": "User Information",
+            "thumbnail": {"url": member.avatar.url},
+            "fields": [
+                {"name": "Display name:", "value": f"{member.global_name}", "inline": False},
+                {"name": "Username:", "value": f"{member.name}", "inline": False},
+                {"name": "Server Name:", "value": member.display_name, "inline": False},
+                {"name": "ID:", "value": member.id, "inline": False},
+                {"name": "Status:", "value": member.raw_status},
+                {"name": "Is active on mobile:", "value": member.is_on_mobile()},
+                {"name": "Is Bot account:", "value": member.bot},
+                {"name": "Account Creation Date:", "value": member.created_at.strftime(date_format), "inline": False},
+                {"name": "Server Join Date:", "value": member.joined_at.strftime(date_format), "inline": False}
+            ],
+            "footer": {"text": f"Command ran on {member.guild}."}
+        }
+        embed = discord.Embed.from_dict(embed_info)
+        await ctx.send(embed=embed)
+        
 async def setup(bot: commands.Bot):
     await bot.add_cog(user(bot))
