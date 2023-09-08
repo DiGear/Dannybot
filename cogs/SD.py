86d0e9cf6cb2e606aaf84300913a384fa6c5b9c9
Danny <zestychair@gmail.com>
2023-09-08 00:18:28 -0400

    move to websockets for API calls

--- cogs/SD.py
+++ cogs/SD.py
@@ -3,124 +3,159 @@ from config import *
 logger = logging.getLogger(__name__)
 
 class sd(commands.Cog):
+    server_address = "127.0.0.1:8188"
+    client_id = str(uuid.uuid4())
+    
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+        self.ws = websocket.WebSocket()
+        self.ws.connect(f"ws://{self.server_address}/ws?clientId={self.client_id}")
+
     @commands.command(name="sd", aliases=["grok", "diffuse"], description="Create AI generated images via Stable-Diffusion.", brief="Create AI generated images with Dannybot.")
-    async def sd(self, ctx, *, positive_prompt:str):       
-        prompt_text = """
-        {
-            "3": {
-                "class_type": "KSampler",
-                "inputs": {
-                    "cfg": 8,
-                    "denoise": 1,
-                    "latent_image": [
-                        "5",
-                        0
-                    ],
-                    "model": [
-                        "4",
-                        0
-                    ],
-                    "negative": [
-                        "7",
-                        0
-                    ],
-                    "positive": [
-                        "6",
-                        0
-                    ],
-                    "sampler_name": "euler",
-                    "scheduler": "normal",
-                    "seed": 8566257,
-                    "steps": 20
-                }
-            },
-            "4": {
-                "class_type": "CheckpointLoaderSimple",
-                "inputs": {
-                    "ckpt_name": "anythingV3_fp16.ckpt"
-                }
-            },
-            "5": {
-                "class_type": "EmptyLatentImage",
-                "inputs": {
-                    "batch_size": 1,
-                    "height": 512,
-                    "width": 512
-                }
-            },
-            "6": {
-                "class_type": "CLIPTextEncode",
-                "inputs": {
-                    "clip": [
-                        "4",
-                        1
-                    ],
-                    "text": "masterpiece best quality girl"
-                }
-            },
-            "7": {
-                "class_type": "CLIPTextEncode",
-                "inputs": {
-                    "clip": [
-                        "4",
-                        1
-                    ],
-                    "text": "bad hands"
-                }
-            },
-            "8": {
-                "class_type": "VAEDecode",
-                "inputs": {
-                    "samples": [
-                        "3",
-                        0
-                    ],
-                    "vae": [
-                        "4",
-                        2
-                    ]
-                }
-            },
-            "9": {
-                "class_type": "SaveImage",
-                "inputs": {
-                    "filename_prefix": "ComfyUI",
-                    "images": [
-                        "8",
-                        0
-                    ]
-                }
+    async def sd(self, ctx, *, positive_prompt: str):
+        generator_values = {
+        "3": {
+            "class_type": "KSampler",
+            "inputs": {
+                "cfg": 7,
+                "denoise": 1,
+                "latent_image": [
+                    "5",
+                    0
+                ],
+                "model": [
+                    "4",
+                    0
+                ],
+                "negative": [
+                    "7",
+                    0
+                ],
+                "positive": [
+                    "6",
+                    0
+                ],
+                "sampler_name": "euler_ancestral",
+                "scheduler": "ddim_uniform",
+                "seed": 8566257,
+                "steps": 20
+            }
+        },
+        "4": {
+            "class_type": "CheckpointLoaderSimple",
+            "inputs": {
+                "ckpt_name": "anythingV3_fp16.ckpt"
+            }
+        },
+        "5": {
+            "class_type": "EmptyLatentImage",
+            "inputs": {
+                "batch_size": 1,
+                "height": 512,
+                "width": 512
+            }
+        },
+        "6": {
+            "class_type": "CLIPTextEncode",
+            "inputs": {
+                "clip": [
+                    "4",
+                    1
+                ],
+                "text": "masterpiece best quality girl"
+            }
+        },
+        "7": {
+            "class_type": "CLIPTextEncode",
+            "inputs": {
+                "clip": [
+                    "4",
+                    1
+                ],
+                "text": "bad hands"
+            }
+        },
+        "8": {
+            "class_type": "VAEDecode",
+            "inputs": {
+                "samples": [
+                    "3",
+                    0
+                ],
+                "vae": [
+                    "4",
+                    2
+                ]
+            }
+        },
+        "9": {
+            "class_type": "SaveImage",
+            "inputs": {
+                "filename_prefix": "ComfyUI",
+                "images": [
+                    "8",
+                    0
+                ]
             }
         }
-        """
+    }
+        prompt = generator_values.copy()
 
-        def queue_prompt(prompt):
-            p = {"prompt": prompt}
-            data = json.dumps(p).encode('utf-8')
-            req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
-            request.urlopen(req)
-
-        prompt = json.loads(prompt_text)
-        seed = random.randint(0, 9999999999)
-        
-        #set the text prompt for our positive CLIPTextEncode
         prompt["6"]["inputs"]["text"] = f"{positive_prompt}"
 
-        #set the seed for our KSampler node
-        prompt["3"]["inputs"]["seed"] = int(seed)
+        prompt["3"]["inputs"]["seed"] = random.randint(0, 9999999999)
 
-        queue_prompt(prompt)
+        images = self.get_images(self.ws, prompt)
 
-        # Wait until comfyui finished generating idk how else to do this rn
-        time.sleep(5)
+        for node_id in images:
+            for image_data in images[node_id]:
+                image = Image.open(io.BytesIO(image_data))
+                out = io.BytesIO()
+                image.save(out, format="png")
+                out.seek(0)
+                await ctx.send(file=discord.File(out, "image.png"))
 
-        list_of_files = glob.glob('I:\\ComfyUI\\ComfyUI\\output/*')
-        latest_file = max(list_of_files, key=os.path.getctime)
-        
-        await ctx.send(f"Generated image for prompt: '{positive_prompt}' using seed: {seed} and model: {prompt['4']['inputs']['ckpt_name']}", file=discord.File(latest_file))
+    def queue_prompt(self, prompt):
+        p = {"prompt": prompt, "client_id": self.client_id}
+        data = json.dumps(p).encode('utf-8')
+        req =  urllib.request.Request(f"http://{self.server_address}/prompt", data=data)
+        return json.loads(urllib.request.urlopen(req).read())
+
+    def get_image(self, filename, subfolder, folder_type):
+        data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
+        url_values = urllib.parse.urlencode(data)
+        with urllib.request.urlopen(f"http://{self.server_address}/view?{url_values}") as response:
+            return response.read()
+    
+    def get_history(self, prompt_id):
+        with urllib.request.urlopen(f"http://{self.server_address}/history/{prompt_id}") as response:
+            return json.loads(response.read())
+
+    def get_images(self, ws, prompt):
+        prompt_id = self.queue_prompt(prompt)['prompt_id']
+        output_images = {}
+        while True:
+            out = ws.recv()
+            if isinstance(out, str):
+                message = json.loads(out)
+                if message['type'] == 'executing':
+                    data = message['data']
+                    if data['node'] is None and data['prompt_id'] == prompt_id:
+                        break
+            else:
+                continue
+
+        history = self.get_history(prompt_id)[prompt_id]
+        for o in history['outputs']:
+            for node_id in history['outputs']:
+                node_output = history['outputs'][node_id]
+                if 'images' in node_output:
+                    images_output = []
+                    for image in node_output['images']:
+                        image_data = self.get_image(image['filename'], image['subfolder'], image['type'])
+                        images_output.append(image_data)
+                output_images[node_id] = images_output
+        return output_images
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(sd(bot))
