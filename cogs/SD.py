613aa7955b2a2dc0d25f7c7a89d99d7e64968420
Danny <zestychair@gmail.com>
2023-09-10 00:37:55 -0400

    optimized the code as well as fixing some minor bugs

--- cogs/SD.py
+++ cogs/SD.py
@@ -54,17 +54,17 @@ class sd(commands.Cog):
         scheduler: Literal[
             "normal", "karras", "exponential", "simple", "ddim_uniform"
         ] = "normal",
-        seed: int = 11223344556677889900112233, #idk how else to do this
+        seed: int = 11223344556677889900112233,  # idk how else to do this
         steps: int = 15,
     ):
         await ctx.defer()
-        if seed == 11223344556677889900112233:
-           seed = random.randint(0, 999999999) 
-        steps = 50 if steps >= 50 else steps
-        height = 1024 if height >= 1024 else height
-        width = 1024 if width >= 1024 else width
-        denoise = 1.000 if denoise >= 1.000 else denoise
-        denoise = 0.001 if denoise <= 0 else denoise
+        seed = (
+            random.randint(0, 999999999) if seed == 11223344556677889900112233 else seed
+        )
+        steps = max(1, min(steps, 50))
+        height = min(max(height, 64), 1024)
+        width = min(max(width, 64), 1024)
+        denoise = min(max(denoise, 0.001), 1.000)
         checkpoints = {
             "Anything": "anythingV3_fp16.ckpt",
             "Sayori (Nekopara) Artstyle": "SayoriDiffusion.ckpt",
@@ -78,8 +78,8 @@ class sd(commands.Cog):
             "3": {
                 "class_type": "KSampler",
                 "inputs": {
-                    "cfg": 7,
-                    "denoise": 1,
+                    "cfg": cfg,
+                    "denoise": denoise,
                     "latent_image": ["5", 0],
                     "model": ["4", 0],
                     "negative": ["7", 0],
@@ -118,54 +118,51 @@ class sd(commands.Cog):
 
         prompt = generator_values.copy()
         images = self.get_images(self.ws, prompt)
-        cfg = prompt["3"]["inputs"]["cfg"]
-        denoise = prompt["3"]["inputs"]["denoise"]
         latent_image = (prompt["5"]["inputs"]["width"], prompt["5"]["inputs"]["height"])
-        model = prompt["4"]["inputs"]["ckpt_name"]
         negative = prompt["7"]["inputs"]["text"]
         positive = prompt["6"]["inputs"]["text"]
-        sampler_name = prompt["3"]["inputs"]["sampler_name"]
-        scheduler = prompt["3"]["inputs"]["scheduler"]
-        seed = prompt["3"]["inputs"]["seed"]
-        steps = prompt["3"]["inputs"]["steps"]
-
+        inputs_values = prompt["3"]["inputs"]
+        cfg, denoise, sampler_name, scheduler, seed, steps = [
+            inputs_values[key]
+            for key in ["cfg", "denoise", "sampler_name", "scheduler", "seed", "steps"]
+        ]
+        embed_fields = [
+            ("Positive Prompt", positive, False),
+            ("Negative Prompt", negative, False),
+            ("CFG Scale", cfg, True),
+            ("Denoise", denoise, True),
+            ("Resolution", f"{latent_image[0]}x{latent_image[1]}", True),
+            ("Checkpoint", checkpoint, True),
+            ("Sampler", sampler_name, True),
+            ("Scheduler", scheduler, True),
+            ("Seed", seed, True),
+            ("Steps", steps, True),
+        ]
         for node_id in images:
             for image_data in images[node_id]:
                 image = Image.open(io.BytesIO(image_data))
-                out = io.BytesIO()
-                image.save(out, format="png")
-                out.seek(0)
-                file = discord.File(out, "image.png")
-                embed = discord.Embed()
-                embed.set_image(url="attachment://image.png")
-                embed.add_field(name="Positive Prompt", value=positive, inline=False)
-                embed.add_field(name="Negative Prompt", value=negative, inline=False)
-                embed.add_field(name="CFG Scale", value=cfg, inline=True)
-                embed.add_field(name="Denoise", value=denoise, inline=True)
-                embed.add_field(
-                    name="Resolution",
-                    value=f"{latent_image[0]}x{latent_image[1]}",
-                    inline=True,
-                )
-                embed.add_field(name="Checkpoint", value=checkpoint, inline=True)
-                embed.add_field(name="Sampler", value=sampler_name, inline=True)
-                embed.add_field(name="Scheduler", value=scheduler, inline=True)
-                embed.add_field(name="Seed", value=seed, inline=True)
-                embed.add_field(name="Steps", value=steps, inline=True)
-
-                await ctx.send(embed=embed, file=file)
+                with io.BytesIO() as out:
+                    image.save(out, format="png")
+                    out.seek(0)
+                    file = discord.File(fp=out, filename="image.png")
+                    embed = discord.Embed()
+                    for name, value, inline in embed_fields:
+                        embed.add_field(name=name, value=value, inline=inline)
+                    embed.set_image(url="attachment://image.png")
+                    await ctx.send(embed=embed, file=file)
 
     def queue_prompt(self, prompt):
-        p = {"prompt": prompt, "client_id": self.client_id}
-        data = json.dumps(p).encode("utf-8")
-        req = urllib.request.Request(f"http://{self.server_address}/prompt", data=data)
-        return json.loads(urllib.request.urlopen(req).read())
+        req = urllib.request.Request(
+            f"http://{self.server_address}/prompt",
+            data=ujson.dumps({"prompt": prompt, "client_id": self.client_id}).encode(
+                "utf-8"
+            ),
+        )
+        return ujson.loads(urllib.request.urlopen(req).read())
 
     def get_image(self, filename, subfolder, folder_type):
-        data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
-        url_values = urllib.parse.urlencode(data)
         with urllib.request.urlopen(
-            f"http://{self.server_address}/view?{url_values}"
+            f"http://{self.server_address}/view?{urllib.parse.urlencode({'filename': filename, 'subfolder': subfolder, 'type': folder_type})}"
         ) as response:
             return response.read()
 
@@ -173,7 +170,7 @@ class sd(commands.Cog):
         with urllib.request.urlopen(
             f"http://{self.server_address}/history/{prompt_id}"
         ) as response:
-            return json.loads(response.read())
+            return ujson.loads(response.read())
 
     def get_images(self, ws, prompt):
         prompt_id = self.queue_prompt(prompt)["prompt_id"]
@@ -181,25 +178,19 @@ class sd(commands.Cog):
         while True:
             out = ws.recv()
             if isinstance(out, str):
-                message = json.loads(out)
-                if message["type"] == "executing":
-                    data = message["data"]
-                    if data["node"] is None and data["prompt_id"] == prompt_id:
+                message = ujson.loads(out)
+                if message.get("type") == "executing":
+                    data = message.get("data")
+                    if data.get("node") is None and data.get("prompt_id") == prompt_id:
                         break
-            else:
-                continue
-
         history = self.get_history(prompt_id)[prompt_id]
-        for o in history["outputs"]:
-            for node_id in history["outputs"]:
-                node_output = history["outputs"][node_id]
-                if "images" in node_output:
-                    images_output = []
-                    for image in node_output["images"]:
-                        image_data = self.get_image(
-                            image["filename"], image["subfolder"], image["type"]
-                        )
-                        images_output.append(image_data)
+        outputs = history.get("outputs", {})
+        for node_id, node_output in outputs.items():
+            if "images" in node_output:
+                images_output = [
+                    self.get_image(image["filename"], image["subfolder"], image["type"])
+                    for image in node_output["images"]
+                ]
                 output_images[node_id] = images_output
         return output_images
 
