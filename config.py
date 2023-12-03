cc0a9ca9371d7846c61ef9ac6cde9f527e5f1259
Danny <zestychair@gmail.com>
2023-12-02 19:38:33 -0500

    audio support

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ import ujson
 import websocket
 import yt_dlp
 from aiohttp import ClientSession
-from discord import File, Interaction, InteractionType, app_commands
+from discord import File, Interaction, InteractionType, app_commands, FFmpegPCMAudio
 from discord.ext import commands, tasks
 from discord.utils import get
 from dotenv import load_dotenv
