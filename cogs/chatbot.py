3ff5b3be4b28caad9e5eb2104b3d387c4a4f48a6
Danny <67078432+FunnyDannyG@users.noreply.github.com>
2024-06-07 21:09:55 -0400

    remove dated gpt models

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -15,7 +15,7 @@ class CustomGPT(commands.FlagConverter):
     presence_penalty: typing.Optional[float] = 0.00
     prompt: str
     model: Literal[
-        "gpt-4o", "gpt-4-turbo-preview", "gpt-4", "gpt-3.5-turbo-0125", "gpt-3.5-turbo"
+        "gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"
     ] = "gpt-4o"
 
 
